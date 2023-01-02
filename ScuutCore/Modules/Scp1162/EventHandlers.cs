8bbe0eb736300d2960b722220fc4a6ee5ec99cd4
Raul125 <71040879+Raul125@users.noreply.github.com>
2023-01-02 17:45:34 +0100

    1162

--- ScuutCore/Modules/Scp1162/EventHandlers.cs
+++ ScuutCore/Modules/Scp1162/EventHandlers.cs
@@ -8,6 +8,9 @@
     using PluginAPI.Core.Attributes;
     using PluginAPI.Enums;
     using PlayerRoles.FirstPersonControl.Spawnpoints;
+    using Mirror;
+    using PluginAPI.Events;
+    using InventorySystem.Items.Pickups;
 
     public class EventHandlers
     {
@@ -15,32 +18,44 @@
         {
         }
 
-        [PluginEvent(ServerEventType.PlayerDropItem)]
-        public bool OnDroppingItem(Player player, ItemBase item)
+        private GameObject Scp1162gameObject = null;
+        [PluginEvent(ServerEventType.RoundStart)]
+        public void OnRoundStarted()
         {
-            try
+            Scp1162gameObject = null;
+            var item = PluginAPI.Core.Items.ItemPickup.Create(ItemType.SCP500, Vector3.zero, default);
+            Scp1162gameObject = item.GameObject;
+            NetworkServer.UnSpawn(item.GameObject);
+
+            item.Rigidbody.transform.parent = GameObject.Find("LCZ_173").transform;
+            item.Rigidbody.useGravity = false;
+            item.Rigidbody.drag = 0f;
+            item.Rigidbody.freezeRotation = true;
+            item.Rigidbody.isKinematic = true;
+            item.GameObject.transform.localPosition = new Vector3(17f, 13.1f, 3f);
+            item.GameObject.transform.localRotation = Quaternion.Euler(90, 1, 0);
+            item.GameObject.transform.localScale = new Vector3(10, 10, 10);
+            NetworkServer.Spawn(item.GameObject);
+        }
+
+        [PluginEvent(ServerEventType.PlayerSearchPickup)]
+        public bool OnPlayerPickup(Player player, ItemPickupBase item)
+        {
+            if (item.gameObject != Scp1162gameObject)
+                return true;
+
+            if (player.CurrentItem != null)
             {
-                RoleSpawnpointManager.TryGetSpawnpointForRole(RoleTypeId.Scp173, out ISpawnpointHandler spawnpoint);
-                spawnpoint.TryGetSpawnpoint(out Vector3 position, out float horizontalRotation);
+                player.ReferenceHub.inventory.ServerRemoveItem(player.CurrentItem.ItemSerial, player.CurrentItem.PickupDropModel);
+                player.AddItem(Scp1162.Instance.Config.Chances.RandomItem());
 
-                if (Vector3.Distance(player.Position, position) <= 8.2f)
-                {
-                    if (Scp1162.Instance.Config.UseHints)
-                        player.ReceiveHint(Scp1162.Instance.Config.ItemDropMessage, Scp1162.Instance.Config.ItemDropMessageDuration);
-                    else
-                        player.SendBroadcast(Scp1162.Instance.Config.ItemDropMessage, Scp1162.Instance.Config.ItemDropMessageDuration, Broadcast.BroadcastFlags.Normal, true);
-
-                    player.ReferenceHub.inventory.ServerRemoveItem(item.ItemSerial, item.PickupDropModel);
-                    ItemType newItemType = Scp1162.Instance.Config.Chances.RandomItem();
-                    PluginAPI.Core.Items.ItemPickup.Create(newItemType, player.Position, Quaternion.identity);
-                    return false;
-                }
-            }
-            catch
-            {
+                if (Scp1162.Instance.Config.UseHints)
+                    player.ReceiveHint(Scp1162.Instance.Config.ItemDropMessage, Scp1162.Instance.Config.ItemDropMessageDuration);
+                else
+                    player.SendBroadcast(Scp1162.Instance.Config.ItemDropMessage, Scp1162.Instance.Config.ItemDropMessageDuration);
             }
 
-            return true;
+            return false;
         }
     }
 }
