03d1fc3a6e174e66ba6df83dac8ff5972f5de722
Raul125 <71040879+Raul125@users.noreply.github.com>
2023-01-14 12:27:09 +0100

    whoops

--- ScuutCore/Modules/Restart/EventHandlers.cs
+++ ScuutCore/Modules/Restart/EventHandlers.cs
@@ -3,6 +3,8 @@
     using GameCore;
     using MEC;
     using PluginAPI.Core;
+    using PluginAPI.Core.Attributes;
+    using PluginAPI.Enums;
     using UnityEngine;
 
     public class EventHandlers
@@ -13,13 +15,15 @@
             restart = rs;
         }
 
-
         public int Rounds = 0;
+
+        [PluginEvent(ServerEventType.RoundRestart)]
         public void OnRoundRestarting()
         {
             Rounds++;
         }
 
+        [PluginEvent(ServerEventType.RoundEnd)]
         public void OnRoundEnd()
         {
             if (Rounds == restart.Config.RestartAfterRounds)
