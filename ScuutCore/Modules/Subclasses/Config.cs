dbd48d15837f3e61b6042cc918239f71eaf9dbb3
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2023-01-19 18:37:32 +0100

    add subclass configs

--- ScuutCore/Modules/Subclasses/Config.cs
+++ ScuutCore/Modules/Subclasses/Config.cs
@@ -1,10 +1,30 @@
 ﻿namespace ScuutCore.Modules.Subclasses
 {
+    using System.Collections.Generic;
+    using PlayerRoles;
     using ScuutCore.API.Interfaces;
+    using ScuutCore.Modules.Subclasses.Models;
 
     public class Config : IModuleConfig
     {
         public bool IsEnabled { get; set; } = true;
         public float SpawnSubclassHintDuration { get; set; } = 5f;
+        public List<SerializedSubclass> Subclasses { get; set; } = new List<SerializedSubclass>()
+        {
+            new SerializedSubclass()
+            {
+                SubclassName = "Janitor",
+                SubclassSpawnChance = 15f,
+                SubclassMaxAlive = 2,
+                SpawnLoadout = new []
+                {
+                    ItemType.KeycardJanitor
+                },
+                RolesToReplace = new []
+                {
+                    RoleTypeId.ClassD
+                }
+            }
+        };
     }
 }
