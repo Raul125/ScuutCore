d8c200b558e573e59593d28a5d92c98d5697c3c0
Raul125 <71040879+Raul125@users.noreply.github.com>
2023-03-10 22:13:44 +0100

    possible fix

--- ScuutCore/Modules/CustomEscape/EventHandlers.cs
+++ ScuutCore/Modules/CustomEscape/EventHandlers.cs
@@ -2,18 +2,14 @@
 {
     using System.Collections.Generic;
     using ScuutCore.API.Features;
-    using InventorySystem.Disarming;
     using MEC;
     using PlayerRoles;
     using PluginAPI.Core;
     using PluginAPI.Core.Attributes;
-    using PluginAPI.Core.Items;
     using PluginAPI.Enums;
-    using UnityEngine;
+
     public sealed class EventHandlers : InstanceBasedEventHandler<CustomEscape>
     {
-        public Vector3 EscapeZone = Vector3.zero;
-
         [PluginEvent(ServerEventType.RoundStart)]
         public void OnRoundStarted()
         {
@@ -25,35 +21,18 @@
 
         private IEnumerator<float> BetterDisarm()
         {
-            for (;;)
+            for (; ; )
             {
-                yield return Timing.WaitForSeconds(1.5f);
+                yield return Timing.WaitForSeconds(.5f);
 
                 foreach (Player player in Player.GetPlayers())
                 {
-                    if (EscapeZone == Vector3.zero)
-                        EscapeZone = Escape.WorldPos;
+                    if (!player.IsDisarmed || !Module.Config.CuffedRoleConversions.TryGetValue(player.Role, out var role) || (Escape.WorldPos - player.Position).sqrMagnitude > Escape.RadiusSqr)
+                            continue;
 
-                    if (!player.ReferenceHub.inventory.IsDisarmed() || (player.Role.GetTeam() != Team.ChaosInsurgency && player.Role.GetTeam() != Team.FoundationForces) || (EscapeZone - player.Position).sqrMagnitude > 400f)
-                        continue;
-
-                    if (!Module.Config.CuffedRoleConversions.TryGetValue(player.Role, out var role))
-                        continue;
-                    var itemList = new List<ItemType>();
-                    foreach (var item in player.Items)
-                        itemList.Add(item.ItemTypeId);
-
-                    Plugin.Coroutines.Add(Timing.RunCoroutine(DropItems(player, itemList)));
                     player.SetRole(role);
                 }
             }
-        }
-
-        private static IEnumerator<float> DropItems(Player player, List<ItemType> items)
-        {
-            yield return Timing.WaitForSeconds(1f);
-            foreach (var item in items)
-                ItemPickup.Create(item, player.Position, default);
         }
     }
 }
