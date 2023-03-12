8813c6f1f19fa7433e8606d3eda94b225883e4b4
Axwabo <axwabo@gmail.com>
2023-03-12 20:07:46 +0100

    drop item debug

---
+++ ScuutCore/Patches/DropItemPatch.cs
@@ -0,0 +1,19 @@
+﻿namespace ScuutCore.Patches
+{
+    using System.Diagnostics;
+    using HarmonyLib;
+    using InventorySystem.Items;
+    using Debug = UnityEngine.Debug;
+    [HarmonyPatch(typeof(ItemBase), nameof(ItemBase.ServerDropItem))]
+    public static class DropItemPatch
+    {
+        public static bool Prefix(ItemBase __instance)
+        {
+            Debug.Log($"drop item of type {__instance.ItemTypeId}");
+            string trace = new StackTrace().ToString();
+            if (trace.Contains("ScuutCore"))
+                Debug.Log(trace);
+            return true;
+        }
+    }
+}
