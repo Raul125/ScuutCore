dc0db78ce95d65188197933c38a463afd089e81c
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2023-03-14 19:51:40 +0100

    add even more info

---
+++ ScuutCore/Patches/CreateItemPatch.cs
@@ -0,0 +1,24 @@
+﻿namespace ScuutCore.Patches
+{
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using HarmonyLib;
+    using InventorySystem;
+    using InventorySystem.Items;
+
+    [HarmonyPatch(typeof(Inventory), nameof(Inventory.CreateItemInstance))]
+    public static class CreateItemPatch
+    {
+        public static void Postfix(ItemBase __result)
+        {
+            if(__result == null)
+                return;
+            ItemCreationCache.Cache.Add(__result, $"stacktrace: {new StackTrace()}, itemtype: {__result.ItemTypeId}, serial: {__result.ItemSerial}, owner: {__result.Owner}");
+        }
+    }
+
+    public static class ItemCreationCache
+    {
+        public static Dictionary<ItemBase, string> Cache = new Dictionary<ItemBase, string>();
+    }
+}
--- ScuutCore/Patches/DropItemPatch.cs
+++ ScuutCore/Patches/DropItemPatch.cs
@@ -13,6 +13,11 @@
                 return true;
             var owner = __instance.Owner;
             Debug.Log($"drop item null pickup: {__instance.ItemTypeId}, serial: {__instance.ItemSerial}, owner: {(owner == null ? "<null>" : owner.nicknameSync.MyNick)}");
+            if (ItemCreationCache.Cache.ContainsKey(__instance))
+            {
+                Debug.Log("Cache contains key");
+                Debug.Log(ItemCreationCache.Cache[__instance]);
+            }
             Debug.Log(new StackTrace().ToString());
             return true;
         }
