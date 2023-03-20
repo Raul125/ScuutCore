59dfe69b96ac461592ff2b4dfa57304579ed1529
Axwabo <axwabo@gmail.com>
2023-03-20 17:11:50 +0100

    fix pocket fall

--- ScuutCore/Modules/PocketFall/EventHandlers.cs
+++ ScuutCore/Modules/PocketFall/EventHandlers.cs
@@ -1,24 +1,23 @@
 ﻿namespace ScuutCore.Modules.PocketFall
 {
-    using ScuutCore.API.Features;
+    using System.Collections.Generic;
+    using API.Features;
+    using CustomPlayerEffects;
+    using MEC;
+    using PlayerStatsSystem;
     using PluginAPI.Core;
-    using UnityEngine;
     using PluginAPI.Core.Attributes;
     using PluginAPI.Enums;
-    using PlayerStatsSystem;
-    using MEC;
-    using System.Collections.Generic;
+    using UnityEngine;
 
     public sealed class EventHandlers : InstanceBasedEventHandler<PocketFall>
     {
         [PluginEvent(ServerEventType.PlayerDamage)]
         public bool OnPlayerDamage(ScuutPlayer target, Player attacker, DamageHandlerBase damageHandler)
         {
-            if (target.EnteringPocket && damageHandler is UniversalDamageHandler dmg 
-                && dmg.TranslationId == DeathTranslations.Falldown.Id)
-                return false;
+            return !target.EnteringPocket || damageHandler is not UniversalDamageHandler dmg
+                                          || dmg.TranslationId != DeathTranslations.Falldown.Id;
 
-            return true;
         }
 
         [PluginEvent(ServerEventType.PlayerEnterPocketDimension)]
@@ -27,26 +26,29 @@
             if (!player.EnteringPocket)
             {
                 player.EnteringPocket = true;
-                Plugin.Coroutines.Add(Timing.RunCoroutine(goPocket(player)));
+                Plugin.Coroutines.Add(Timing.RunCoroutine(SendToPocket(player)));
             }
 
             return false;
         }
 
-        private IEnumerator<float> goPocket(ScuutPlayer player)
+        private IEnumerator<float> SendToPocket(ScuutPlayer player)
         {
             yield return Timing.WaitForSeconds(Module.Config.Delay);
 
             for (int i = 0; i < Module.Config.Ticks; i++)
             {
-                player.Position -= Vector3.down;
+                player.Position += Vector3.down;
                 yield return Timing.WaitForOneFrame;
             }
 
             if (Warhead.IsDetonated)
                 player.Kill();
             else
+            {
                 player.SendToPocketDimension();
+                player.EffectsManager.EnableEffect<Corroding>();
+            }
 
             yield return Timing.WaitForSeconds(0.1f);
             player.EnteringPocket = false;
