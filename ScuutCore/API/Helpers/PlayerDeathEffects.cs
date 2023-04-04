e45438e13df337cc052e9993bcd2fe209e37d7d2
Axwabo <axwabo@gmail.com>
2023-04-04 17:35:18 +0200

    simplify explosion effect

--- ScuutCore/API/Helpers/PlayerDeathEffects.cs
+++ ScuutCore/API/Helpers/PlayerDeathEffects.cs
@@ -1,38 +1,17 @@
 ﻿namespace ScuutCore.API.Helpers
 {
-    using Footprinting;
-    using InventorySystem;
-    using InventorySystem.Items.Pickups;
-    using InventorySystem.Items.ThrowableProjectiles;
-    using Mirror;
+    using InventorySystem.Items.Usables.Scp330;
     using PluginAPI.Core;
-    using UnityEngine;
+    using Utils.Networking;
 
     public static class PlayerDeathEffects
     {
-        // wtf
         public static void PlayExplosionEffect(Player ply)
         {
-            ThrowableItem throwableItem = (ThrowableItem)ReferenceHub._hostHub.inventory.ServerAddItem(ItemType.GrenadeHE, 0, null);
-            ThrownProjectile thrownProjectile = UnityEngine.Object.Instantiate<ThrownProjectile>(throwableItem.Projectile, ply.Position, Quaternion.identity);
-            ((ExplosionGrenade)thrownProjectile)._fuseTime = 0.1f;
-            PickupSyncInfo pickupSyncInfo = new PickupSyncInfo(throwableItem.ItemTypeId, ply.Position, Quaternion.identity, throwableItem.Weight, throwableItem.ItemSerial)
+            new CandyPink.CandyExplosionMessage
             {
-                Locked = true
-            };
-            PickupSyncInfo pickupSyncInfo2 = pickupSyncInfo;
-            thrownProjectile.NetworkInfo = pickupSyncInfo2;
-            thrownProjectile.PreviousOwner = new Footprint(throwableItem.Owner);
-            (thrownProjectile as ExplosionGrenade)._maxRadius = 0f;
-            (thrownProjectile as ExplosionGrenade)._scpDamageMultiplier = 0f;
-            NetworkServer.Spawn(thrownProjectile.gameObject);
-            ItemPickupBase itemPickupBase = thrownProjectile;
-            pickupSyncInfo2 = default;
-            ItemPickupBase itemPickupBase2 = itemPickupBase;
-            pickupSyncInfo = default;
-            itemPickupBase2.InfoReceived(pickupSyncInfo, pickupSyncInfo2);
-            thrownProjectile.ServerActivate();
-            ReferenceHub._hostHub.inventory.ServerRemoveItem(throwableItem.ItemSerial, null);
+                Origin = ply.Position
+            }.SendToAuthenticated();
         }
     }
 }
