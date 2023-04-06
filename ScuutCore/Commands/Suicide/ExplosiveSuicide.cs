f38bf4dfa71101d0fc7ec1fe1353fd62251882d0
Axwabo <axwabo@gmail.com>
2023-04-06 10:03:44 +0200

    explosive suicide ragdoll fly

--- ScuutCore/Commands/Suicide/ExplosiveSuicide.cs
+++ ScuutCore/Commands/Suicide/ExplosiveSuicide.cs
@@ -1,53 +1,52 @@
 ﻿namespace ScuutCore.Commands.Suicide
 {
-	using System;
-	using CommandSystem;
-	using Footprinting;
-	using InventorySystem;
-	using InventorySystem.Items.Pickups;
-	using InventorySystem.Items.ThrowableProjectiles;
-	using Mirror;
-	using PluginAPI.Core;
-	using ScuutCore.API.Helpers;
-	using UnityEngine;
-	using PermissionHandler = NWAPIPermissionSystem.PermissionHandler;
+    using System;
+    using CommandSystem;
+    using PluginAPI.Core;
+    using ScuutCore.API.Helpers;
+    using PermissionHandler = NWAPIPermissionSystem.PermissionHandler;
 
-	[CommandHandler(typeof(ClientCommandHandler))]
-	public class ExplosiveSuicide : ICommand
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class ExplosiveSuicide : ICommand
     {
+        public const string DeathReason = "Ate taco bell";
+
         public string Command { get; } = "explosivesuicide";
 
-		public string[] Aliases { get; } = new []{"explode"};
+        public string[] Aliases { get; } = new[]
+        {
+            "explode"
+        };
 
-		public string Description { get; } = "Explode";
+        public string Description { get; } = "Explode";
 
-		public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
-		{
-			Player player = Player.Get(sender);
-			if (!PermissionHandler.CheckPermission(player.UserId, "scuutcore.explosivesuicide"))
-			{
-				response = "<b><color=#00FFAE>Get permissions bozo!</color></b>";
-				return false;
-			}
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get(sender);
+            if (!PermissionHandler.CheckPermission(player.UserId, "scuutcore.explosivesuicide"))
+            {
+                response = "<b><color=#00FFAE>Get permissions bozo!</color></b>";
+                return false;
+            }
 
-			if (!Round.IsRoundStarted)
-			{
-				response = "You gotta wait for the round to start!";
-				return false;
-			}
+            if (!Round.IsRoundStarted)
+            {
+                response = "You gotta wait for the round to start!";
+                return false;
+            }
 
-			if (Plugin.Singleton.Config.SuicideDisabledRoles.Contains(player.Role))
-			{
-				response = "Disabled for this role";
-				return false;
-			}
+            if (Plugin.Singleton.Config.SuicideDisabledRoles.Contains(player.Role))
+            {
+                response = "Disabled for this role";
+                return false;
+            }
 
-			PlayerDeathEffects.PlayExplosionEffect(player);
+            PlayerDeathEffects.PlayExplosionEffect(player);
 
-			player.Kill("Ate taco bell");
+            player.Kill(DeathReason);
 
-			response = "Done";
-			return true;
-		}
+            response = "Done";
+            return true;
+        }
     }
 }
