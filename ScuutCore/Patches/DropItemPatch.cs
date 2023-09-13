b181792a38b74ae244f2432055c7e8fa9212599b
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2023-09-14 00:26:24 +0200

    add check

---
+++ ScuutCore/Patches/DropItemPatch.cs
@@ -0,0 +1,22 @@
+﻿namespace ScuutCore.Patches
+{
+    using HarmonyLib;
+    using InventorySystem.Items;
+    using PluginAPI.Core;
+
+    [HarmonyPatch(typeof(ItemBase), nameof(ItemBase.ServerDropItem))]
+    public static class DropItemPatch
+    {
+        public static bool Prefix(ItemBase __instance)
+        {
+            if (__instance.PickupDropModel == null)
+            {
+                var stackTrace = new System.Diagnostics.StackTrace();
+                Log.Error("PickupDropModel is null! Stack trace: " + stackTrace);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
