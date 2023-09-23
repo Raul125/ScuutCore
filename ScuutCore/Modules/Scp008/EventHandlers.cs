312b3b4eaffb3523d5b0e34abc8e2c4ab2da77b8
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2023-09-23 21:04:21 +0200

    fix posion

--- ScuutCore/Modules/Scp008/EventHandlers.cs
+++ ScuutCore/Modules/Scp008/EventHandlers.cs
@@ -31,9 +31,13 @@
         [PluginEvent(ServerEventType.PlayerUsedItem)]
         public void OnHealed(Player player, ItemBase item)
         {
+            if (player.EffectsManager.TryGetEffect(out Scp207 scp027)
+                && scp027.IsEnabled
+                && player.EffectsManager.TryGetEffect(out Scp1853 scp1853)
+                && scp1853.IsEnabled)
+                return;
             if (player.EffectsManager.TryGetEffect<Poisoned>(out var poisonedEffect) && !poisonedEffect.IsEnabled)
                 return;
-
             if (!Module.Config.CureChance.TryGetValue(item.ItemTypeId, out int chance) || Random.Range(0, 100) > chance)
                 return;
             poisonedEffect.DisableEffect();
