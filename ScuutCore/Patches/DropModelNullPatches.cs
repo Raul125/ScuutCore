82393deaf457565a558c1c9025b94852ed9065b3
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2023-09-24 21:26:32 +0200

    replace spam with coin

--- ScuutCore/Patches/DropModelNullPatches.cs
+++ ScuutCore/Patches/DropModelNullPatches.cs
@@ -1,22 +1,29 @@
 ﻿namespace ScuutCore.Patches
 {
     using System;
+    using Footprinting;
     using HarmonyLib;
     using InventorySystem;
     using InventorySystem.Items;
     using InventorySystem.Items.Pickups;
+    using InventorySystem.Items.Usables.Scp1576;
+    using Mirror;
     using PluginAPI.Core;
     using UnityEngine;
+    using Object = UnityEngine.Object;
 
     [HarmonyPatch(typeof(ItemBase), nameof(ItemBase.ServerDropItem))]
     public static class DropItemPatch
     {
-        public static bool Prefix(ItemBase __instance)
+        public static bool Prefix(ItemBase __instance, ref ItemPickupBase __result)
         {
             if (__instance.PickupDropModel == null)
             {
                 var stackTrace = new System.Diagnostics.StackTrace();
                 Log.Error($"PickupDropModel is null! type:{__instance.ItemTypeId} Stack trace: " + stackTrace);
+                __instance.Owner.inventory.UserInventory.Items.Remove(__instance.ItemSerial);
+                Object.Destroy(__instance.gameObject);
+                __result = DropModelNullHelper.CreateCoinPickup(__instance.Owner);
                 return false;
             }
 
@@ -29,16 +36,42 @@
     public static class ServerCreatePickupPatch
     {
         public static bool Prefix(ItemBase item, PickupSyncInfo psi, Vector3 position, bool spawn,
-            Action<ItemPickupBase> setupMethod)
+            Action<ItemPickupBase> setupMethod, ref ItemPickupBase __result)
         {
             if (item.PickupDropModel == null)
             {
                 var stackTrace = new System.Diagnostics.StackTrace();
                 Log.Error($"PickupDropModel is null! type:{item.ItemTypeId} Stack trace: " + stackTrace);
+                __result = DropModelNullHelper.CreateCoinPickup(item.Owner, position, spawn);
                 return false;
             }
 
             return true;
+        }
+    }
+
+    internal static class DropModelNullHelper
+    {
+        internal static ItemPickupBase CreateCoinPickup(ReferenceHub? owner = null, Vector3? position = null, bool spawn = false)
+        {
+            owner ??= ReferenceHub.HostHub;
+            var @base = InventoryItemLoader.AvailableItems[ItemType.Coin];
+            var item = Object.Instantiate(@base.PickupDropModel);
+
+            PickupSyncInfo psi = new()
+            {
+                ItemId = ItemType.Coin,
+                Serial = ItemSerialGenerator.GenerateNext(),
+                WeightKg = @base.Weight
+            };
+
+            item.Info = psi;
+            item.PreviousOwner = new Footprint(owner);
+            if (position.HasValue)
+                item.Position = position.Value;
+            if (spawn)
+                NetworkServer.Spawn(item.gameObject);
+            return item;
         }
     }
 }
