b2e43f367a589deafe1a663241afeb6d33db8d09
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2023-09-27 21:34:13 +0200

    add commahdnaler

---
+++ ScuutCore/Modules/Patreon/Commands/PatreonChatCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace ScuutCore.Modules.Patreon.Commands
+{
+    using System;
+    using CommandSystem;
+    using ScuutCore.API.Helpers;
+
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class PatreonChatCommand : PatreonExclusiveCommand
+    {
+        public const string PatreonChatCommandPermissions = "scuutcore.patreon.spectatorlist";
+
+        protected override string Permission => PatreonChatCommandPermissions;
+        public override string Command => "patreonchat";
+        public override string[] Aliases { get; } = { };
+        public override string Description => "Write in patreonchat.";
+
+        protected override bool ExecuteInternal(ArraySegment<string> arguments, ReferenceHub sender, PatreonData data, out string response)
+        {
+            var senderName = sender.nicknameSync.MyNick ?? "null";
+            GlobalHelpers.BroadcastToPermissions("patreonchat", $"[PCHAT] ({senderName}): {string.Join(" ", arguments)}");
+            response = "Sent";
+            return true;
+        }
+    }
+}
