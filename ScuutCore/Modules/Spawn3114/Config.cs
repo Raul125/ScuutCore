319b776a4df589f820ccdf32ceda019d6385c1b7
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2023-11-14 22:22:02 +0100

    add spawn3114 module

---
+++ ScuutCore/Modules/Spawn3114/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace ScuutCore.Modules.Spawn3114;
+
+using ScuutCore.API.Interfaces;
+
+public class Config : IModuleConfig
+{
+    public bool IsEnabled { get; set; } = true;
+    public float Chance { get; set; } = 0.5f;
+    public int MaxScp3114 { get; set; } = 2;
+}
---
+++ ScuutCore/Modules/Spawn3114/Spawn3114.cs
@@ -0,0 +1,38 @@
+﻿namespace ScuutCore.Modules.Spawn3114;
+
+using System;
+using PlayerRoles;
+using PlayerRoles.PlayableScps.Scp3114;
+using PlayerRoles.RoleAssign;
+using ScuutCore.API.Features;
+
+public class Spawn3114 : Module<Config>
+{
+    public override void OnEnabled()
+    {
+        RoleAssigner.OnPlayersSpawned += Scp3114Spawner.OnPlayersSpawned;
+        base.OnEnabled();
+    }
+
+    public override void OnDisabled()
+    {
+        RoleAssigner.OnPlayersSpawned -= Scp3114Spawner.OnPlayersSpawned;
+        base.OnDisabled();
+    }
+
+    private void OnPlayersSpawned()
+    {
+        Scp3114Spawner._ragdollsSpawned = false;
+        if (UnityEngine.Random.value >= Config.Chance)
+        {
+            return;
+        }
+        Scp3114Spawner.SpawnCandidates.Clear();
+        PlayerRolesUtils.ForEachRole<HumanRole>(Scp3114Spawner.SpawnCandidates.Add);
+        if (Scp3114Spawner.SpawnCandidates.Count < Config.MaxScp3114)
+        {
+            return;
+        }
+        Scp3114Spawner.SpawnCandidates.RandomItem<ReferenceHub>().roleManager.ServerSetRole(RoleTypeId.Scp3114, RoleChangeReason.RoundStart);
+    }
+}
